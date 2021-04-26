718c75d84cf6562df32460134fcf2faf4cf15562
Ionut Predoi <ionut@wordskii.com>
2021-04-26 15:28:52 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'to-do-list';
+  todos = [
+    { label: 'Bring Milk', done: false, priority: 3 },
+    { label: 'Pay Mobile Bill', done: true, priority: 2 },
+    { label: 'Clean House', done: false, priority: 2 },
+    { label: 'Feed Cat', done: true, priority: 1 },
+  ];
+
+  newTodo?: { label: string; done: boolean; priority: number };
+
+  addTodo(newTodoLabel: string) {
+    this.newTodo = { label: newTodoLabel, done: false, priority: 2 };
+    this.todos.push(this.newTodo);
+  }
 }
